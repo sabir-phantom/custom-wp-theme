3d5ba93ad344e18e57892d9318f18ff81cb51eef
sabir-phantom <sabir.sbair@gmail.com>
2023-11-01 17:08:09 +0600

    fixed wp_footer

--- functions.php
+++ functions.php
@@ -15,7 +15,7 @@ function customtheme_register_scripts() {
     wp_enqueue_script('customtheme-jquery', 'https://code.jquery.com/jquery-3.4.1.slim.min.js', array(), '3.4.1', true) ;
     wp_enqueue_script('customtheme-popper', 'https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js', array(), '1.16.0', true) ;
     wp_enqueue_script('customtheme-bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js', array(), '4.4.1', true) ;
-    wp_enqueue_script('customtheme-bootstrap', get_template_directory_uri()."/assets/js/main.js", array(), '1.0', true) ;
+    wp_enqueue_script('customtheme-main', get_template_directory_uri()."/assets/js/main.js", array(), '1.0', true) ;
 }
 
 add_action('wp_enqueue_scripts','customtheme_register_scripts') ;
